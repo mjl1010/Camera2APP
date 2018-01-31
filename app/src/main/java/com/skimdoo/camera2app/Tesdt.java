b10524c951cce23313e78a98a974437e5652e2c6
mjl1010 <mjl1010@gmx.es>
2018-01-31 18:40:02 +0100

    Initial commit with Errors!

---
+++ app/src/main/java/com/skimdoo/camera2app/Tesdt.java
@@ -0,0 +1,13 @@
+package com.skimdoo.camera2app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Tesdt extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tesdt);
+    }
+}
